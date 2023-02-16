51fc983ce61ebf701d4468d2ffd1d1d4846f4da0
LeoAraujo30 <leonardo.araujo300301@gmail.com>
2023-02-16 17:37:56 -0300

    requisitos 3 e 7 feitos

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,37 +1,19 @@
 from typing import List, Dict
+import csv
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
+    with open(path, mode="r") as file:
+        jobs = csv.DictReader(file, delimiter=",", quotechar='"')
+        list = []
+        for job in jobs:
+            if job["industry"] not in list and job["industry"] != "":
+                list.append(job["industry"])
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+        return list
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
-    """Filters a list of jobs by industry
+    list = [job for job in jobs if job["industry"] == industry]
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    raise NotImplementedError
+    return list
